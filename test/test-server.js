3aec44c444bacef5f4c46053634a673fd093f7a3
Melody Plan <melodyplan@gmail.com>
2017-06-29 16:04:49 -0700

    testing w travis

---
+++ test/test-server.js
@@ -0,0 +1,88 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const {app, runServer, closeServer} = require('../server');
+
+const should = chai.should();
+
+chai.use(chaiHttp);
+
+describe('Blog API', function() {
+  before(function() {
+    return runServer();
+  });
+
+  after(function() {
+    return closeServer();
+  });
+
+  it('should list blog posts on GET', function() {
+    return chai.request(app)
+    .get('/blog-posts')
+    .then(function(res) {
+      res.should.have.status(200);
+      res.should.be.json;
+      res.body.should.be.a('array');
+
+      res.body.length.should.be.at.least(1);
+
+      const expectedKeys = ['title', 'content', 'author'];
+      res.body.forEach(function(item) {
+        item.should.be.a('object');
+        item.should.include.keys(expectedKeys);
+      });
+    });
+  });
+
+  it('should add a blog post on POST', function() {
+    const newBlogPost = {title: 'Cool Posts Only', content: 'Unbelievable cool content', author: 'A Cool Person'};
+    return chai.request(app)
+      .post('/blog-posts')
+      .send(newBlogPost)
+      .then(function(res) {
+        res.should.have.status(201);
+        res.should.be.json;
+        res.body.should.be.a('object');
+        res.body.should.include.keys('title', 'content', 'author');
+        res.body.id.should.not.be.null;
+
+        res.body.should.deep.equal(Object.assign(newBlogPost, {res.body.id}));
+      });
+  });
+
+  it('should update blog posts on PUT', function() {
+    const updateBlogPost = {
+      title: 'An Open Letter to Nintendo',
+      content: 'Will Yoshi get the recognition he deserves?',
+      author: 'Coalition Against Virtual Dinosaur Abuse'
+    };
+
+    return chai.request(app)
+      .get('/blog-posts/:id')
+      .then(function(res) {
+        updateBlogPost.id = res.body[0].id;
+        return chai.request(app)
+          .put(`/blog-posts/:id/${updateBlogPost.id}`)
+          .send(updateBlogPost);
+      })
+      .then(function(res) {
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('object');
+        res.body.should.deep.equal(updateBlogPost);
+      });
+  });
+
+  it('should delete blog posts on DELETE', function() {
+    return chai.request(app)
+      .get('/blog-posts/:id')
+      .then(function(res) {
+        return chai.request(app)
+          .delete(`/blog-posts/:id/${res.body[0].id}`);
+      })
+      .then(function(res) {
+        res.should.have.status(204);
+      });
+  });
+  
+});
