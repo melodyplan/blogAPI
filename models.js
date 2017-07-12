bbe1f1a16dd2430c05b3f10dabcad847df4dbdc6
Melody Plan <melodyplan@gmail.com>
2017-07-12 07:04:19 -0700

    moved stuff and made things mongoose

---
+++ models.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+
+const blogSchema = mongoose.Schema({
+  title: {type: String, required: true},
+  content: {type: String, required: true},
+  author: {
+    firstName: String,
+    lastName: String
+  }
+});
+
+blogSchema.virtual('authorName').get(function() {
+  return `${this.author.firstName} ${this.author.lastName}`.trim()});
+
+
+blogSchema.methods.apiRepr = function() {
+  return {
+    id: this._id,
+    title: this.title,
+    content: this.content,
+    author: this.authorName
+  };
+}
+
+const Blog = mongoose.model('Blog', blogSchema);
+
+module.exports = {Blog};
--- server.js
+++ server.js
@@ -1,36 +1,136 @@
-
+const bodyParser = require('body-parser');
 const express = require('express');
-const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+const {PORT, DATABASE_URL} = require('./config')
+const {Blog} = require('./models')
 
 const app = express();
+app.use(bodyParser.json());
 
-const blogPostsRouter = require('./routes/blogPostsRouter'); // router
+app.get('/posts', (req, res) => {
+  Blog
+    .find()
+    .exec()
+    .then(blogs => {
+      res.json({
+        blogs: blogs.map(
+          (blog) => blog.apiRepr())
+      });
+    })
+    .catch(
+      err => {
+        console.error(err);
+        res.status(400).json({message: 'Bad request error: title, content or author name is missing'});
+      });
+});
 
-app.use('/blog-posts', blogPostsRouter); // http://localhost:8080/blog-posts/
+app.get('/posts/:id', (req, res) => {
+  Blog
+    .findById(req.params.id)
+    .exec()
+    .then(blog => res.json(blog.apiRepr()))
+    .catch(err => {
+      console.error(err);
+        res.status(400).send(message);
+    });
+});
+
+app.post('/post', (req, res) => {
+  const requiredFields = ['title', 'content', 'author'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \'${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+
+  Blog
+    .create({
+      title: req.body.title,
+      content: req.body.content,
+      author: req.body.author})
+    .then(
+      blog => res.status(200).json(blog.apiRepr()))
+    .catch(err => {
+      console.error(err);
+      res.status(400).json({message: 'Bad request error'});
+    });
+});
+
+app.put('/posts/:id', (req, res) => {
+  if (!(req.parms.id && req.body.id && req.params.id === req.body.id)) {
+    const message = (
+      `Request path id (${req.params.id}) and request body id ` +
+      `(${req.body.id}) must match`);
+    console.error(message);
+    res.status(400).json({message: message});
+  }
+
+  const toUpdate = {};
+  const updateableFields = ['name', 'content', 'author'];
+
+  updateableFields.forEach(field => {
+    if (field in req.body) {
+      toUpdate[field] = req.body[field];
+    }
+  });
+
+  Blog
+    .findByIdAndUpdate(req.params.id, {$set: toUpdate})
+    .exec()
+    .then(blog => res.status(200).end())
+    .catch(err => res.status(400).json({message: 'Bad request error'}));
+});
+
+app.delete('/restaurants/:id', (req, res) => {
+  Blog
+    .findByIdAndRemove(req.params.id)
+    .exec()
+    .then(blog => res.status(204).end())
+    .catch(err => res.status(400).json({message: 'Bad request error'}));
+});
+
+//catch-all endpoint is client makes request
+app.use('*', function(req, res) {
+  res.status(404).json({messgae: 'Not Found'})
+});
 
 let server;
 
-function runServer() {
-  const port = process.env.PORT || 8080;
+function runServer(databaseUrl = DATABASE_URL, port = PORT) {
   return new Promise((resolve, reject) => {
-    server = app.listen(port, () => {
-      console.log(`Your app is listening on port ${port}`);
-      resolve(server);
-    }).on('error', err => {
-      reject(err)
+    mongoose.connect(databaseUrl, err => {
+      if (err) {
+        return reject(err);
+      }
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on ${port}`);
+        resolve();
+      })
+      .on('error', err => {
+        mongoose.disconnect();
+        reject(err);
+      });
     });
   });
 }
 
+//close server and return promise
 function closeServer() {
-  return new Promise((resolve, reject) => {
-    console.log('Closing server');
-    server.close(err => {
-      if (err) {
-        reject(err);
-        return;
-      }
-      resolve();
+  return mongoose.disconnect().then(() => {
+    return new Promise((resolve, reject) => {
+      console.log('Closing server');
+      server.close(err => {
+        if (err) {
+          return reject(err);
+        }
+        resolve();
+      });
     });
   });
 }
@@ -39,9 +139,4 @@ if (require.main === module) {
   runServer().catch(err => console.error(err));
 };
 
-module.exports = {app, runServer, closeServer};
-
-
-// app.listen(process.env.PORT || 8080, () => {
-//   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
-// });
+module.exports = {ap, runServer, closeServer};
